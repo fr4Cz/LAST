b6c44acc7b56381c79291e961e3018e79a7eacf9
MOCHZ <contact@zorko.co>
2019-07-11 22:33:45 +0200

    Added wget

--- lib/webfetch.py
+++ lib/webfetch.py
@@ -4,16 +4,33 @@ import wget
 import json
 
 
-class WebFetch():
+class WebFetch:
     def __init__(self):
-        try:
-            if os.path.isfile('conf/wget.cfg'):
+        if os.path.isfile('conf/wget.cfg'):
+            try:
                 self.config = json.loads(open('conf/wget.cfg', 'r').read())
-        except json.JSONDecodeError as e:
-            # log.warn(e)
-            pass
+            except json.JSONDecodeError as e:
+                # log.warn(e)
+                print('JSON Error: {}'.format(e))
 
     def get(self):
-        for pkg in self.config:
-            # Todo; implement wget
+        try:
+            for pkg in self.config:
+                try:
+                    print('Downloading: {}'.format(pkg['name']))
+                    if not os.path.isdir(pkg['destination']):
+                        try:
+                            os.makedirs(pkg['destination'])
+                        except OSError as e:
+                            # log.err(e)
+                            print(e)
+                            pass
+                    wget.download(pkg['url'], out=pkg['destination'])
+                except Exception as e:
+                    # log.warn(e)
+                    print(e)
+                    pass
+        except Exception as e:
+            # log.err(e)
+            print(e)
             pass
